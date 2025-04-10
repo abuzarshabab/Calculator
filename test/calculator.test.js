f23178c889949084b5709143d8eb11df2b9dae42
abuzarshabab <abuzarshabab@gmail.com>
2025-04-10 19:37:35 +0530

    Added single number test case

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,8 +4,13 @@ import { describe, it } from 'mocha';
 
 import { add } from '../src/calculator.js';
 describe('Text Calculator', () => {
-  it ('Should return 0, for not input', () => {
+  it ('Should return 0, for empty string as input', () => {
     const result = add('')
     expect(result).to.equal(0)
   })
+
+  it('Should return the number itself when single value is passed', () => {
+    const result = add('7');
+    expect(result).to.equal(7);
+  });
 })
